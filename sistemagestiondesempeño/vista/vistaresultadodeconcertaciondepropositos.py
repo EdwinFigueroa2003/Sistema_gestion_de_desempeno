6da3068da504f140fd49a804127eb0a66b9d8c41
Edwin j Figueroa <juniordelacuesta37@gmail.com>
2024-09-18 15:40:47 -0500

    Resultado concertacion de propositos

--- sistemagestiondesempeño/vista/vistaresultadodeconcertaciondepropositos.py
+++ sistemagestiondesempeño/vista/vistaresultadodeconcertaciondepropositos.py
@@ -1,15 +1,24 @@
-from pprint import pprint
-from flask import Blueprint, request, render_template, redirect, url_for, session
-from Entidad import Entidad
+from flask import Blueprint, request, render_template
 import requests
-from control.ControlEntidad import ControlEntidad
-from configBd import API_URL
  
-# Crear un Blueprint
 vistaresultadosconcertaciondepropositos = Blueprint('idresultadosconcertaciondepropositos', __name__, template_folder='templates')
  
-@vistaresultadosconcertaciondepropositos.route('/resultadosconcertaciondepropositos', methods=['GET', 'POST'])
+@vistaresultadosconcertaciondepropositos.route('/resultadosconcertaciondepropositos', methods=['GET'])
 def vista_resultadosconcertaciondepropositos():
-    
-
-    return render_template('resultadosconcertaciondepropositos.html')
+    # Obtener el parámetro de categoría desde la URL
+    categoria = request.args.get('categoria', None)
+    try:
+        # Usar la URL correcta con el formato especificado
+        if categoria:
+            response = requests.get(f'http://127.0.0.1:5184/api/sgd/proposito/id_categoria/{categoria}')
+        else:
+            response = requests.get(f'http://127.0.0.1:5184/api/sgd/proposito')
+ 
+        response.raise_for_status()  # Lanza una excepción si hay un error
+        propositos = response.json()  # Parsear la respuesta JSON
+    except requests.RequestException as e:
+        print(f"Error al obtener los datos de la API: {e}")
+        propositos = []
+ 
+    # Renderizar la plantilla y pasar los propósitos obtenidos
+    return render_template('resultadosconcertaciondepropositos.html', propositos=propositos)
